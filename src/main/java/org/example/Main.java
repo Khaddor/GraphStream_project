5680636d46362f81755b74de43ccb49cea49f507
El Mehdi Khaddor <el-mehdi.khaddor@etu.univ-lehavre.fr>
2022-11-26 21:18:02 +0100

    Added the random Generator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.algorithm.generator.RandomGenerator;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+
     }
 }
---
+++ src/main/java/org/example/RandomGen.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.algorithm.generator.RandomGenerator;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
+import java.util.Random;
+
+public class RandomGen {
+    private String GraphName;
+    private int n,d;
+
+    public RandomGen(String GraphName, int n, int d){
+        this.GraphName = GraphName;
+        this.n = n;
+        this.d = d;
+    }
+
+    public Graph generate(){
+        Graph graph = new SingleGraph(GraphName);
+        Generator generator = new RandomGenerator(this.d);
+        Random r = new Random();
+
+        generator.addSink(graph);
+        generator.begin();
+        for(int i =0; i<d; i++){
+            generator.nextEvents();
+        }
+        generator.end();
+
+        graph.edges().forEach(e -> {
+            e.setAttribute("poids" , r.nextInt(12) );
+            e.setAttribute("ui.label" , e.getAttribute("poids"));
+        });
+
+        return graph;
+    }
+
+}
