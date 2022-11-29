f90bdbb8a33dda01cbfa63099a829619d81aec11
El Mehdi Khaddor <el-mehdi.khaddor@etu.univ-lehavre.fr>
2022-11-29 13:04:02 +0100

    Added GraphStream's Djikistra

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import org.graphstream.algorithm.Dijkstra;
 import org.graphstream.algorithm.generator.Generator;
 import org.graphstream.algorithm.generator.RandomGenerator;
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
 
 import java.util.Random;
@@ -10,12 +12,23 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-
+        //Génération d'un graphe aléatoire
         RandomGen randomGen = new RandomGen("myGraph", 3,5);
         Graph graph = randomGen.generate();
-        
+
+        //Ma version du Djikistra
        MyDjikistra myDjikistra = new MyDjikistra(graph, "0");
        graph.display();
+
+
+       //Djikistra du GraphStream
+        Dijkstra dijkstra = new Dijkstra(Dijkstra.Element.EDGE,"result", "poids");
+        dijkstra.init(graph);
+        dijkstra.setSource("0");
+        dijkstra.compute();
+        System.out.println("---------------Djikistra du GraphStream---------");
+        for (Node node : graph)
+            System.out.printf("%s->%s:%6.2f%n", dijkstra.getSource(), node, dijkstra.getPathLength(node));
 
     }
 }
--- src/main/java/org/example/MyDjikistra.java
+++ src/main/java/org/example/MyDjikistra.java
@@ -6,49 +6,47 @@ import org.graphstream.graph.Node;
 
 import java.util.HashMap;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toList;
-
-import java.util.Collections;
 public class MyDjikistra {
 
     Graph graph;
     String source;
     Node minNode,thisNode = null;
-    int min=999, poids;
+    int poids;
 
     public MyDjikistra(Graph graph, String source){
         this.graph = graph;
         this.source = source;
-       
 
-        HashMap<Node,Integer> visited = new HashMap<>();
+        //Liste des noueds non parcourus
         HashMap<Node,Integer> nonVisited = new HashMap<>();
 
-        //Distance = infini pour tous les noeud non visites
+        //Liste des noeuds dèjà parcourus
+        HashMap<Node,Integer> visited = new HashMap<>();
+
+        //Initialiser la distance par l'infini pour tous les noeuds
         graph.nodes().forEach(e -> {
             nonVisited.put(e, Integer.MAX_VALUE);
         });
 
-        //Distance = 0 pour le noeud source
+        //Initialiser le noued source par 0
         nonVisited.replace(graph.getNode(source), 0);
 
+        System.out.println("--------Ma version du Djikistra----------------");
+        //parcourir tous les noeuds dans la liste des noueds non visités
         while(!nonVisited.isEmpty()){
-        	
+
+        	//Extraire le noeuds avec la distance minimale
         	thisNode = getMinNode(nonVisited);
-        	System.out.println(thisNode);
-        	
-        	
+
         	
             if( thisNode == null){
                 //nonVisited.forEach((n,i) -> visited.put(n,i));
                 visited.put(thisNode,nonVisited.get(thisNode));
                 nonVisited.remove(thisNode);
-                System.out.println("hna");
                 
             }else{
-            	
+                    //Parcourir tous les noeuds voisins du Nouveau noeud
             		thisNode.neighborNodes().forEach( v -> {
             			poids = (int) thisNode.getEdgeBetween(v).getAttribute("poids");
             				if(nonVisited.containsKey(thisNode) && nonVisited.containsKey(v)){
@@ -56,43 +54,33 @@ public class MyDjikistra {
 	                    				nonVisited.replace(v,nonVisited.get(thisNode) + poids);
 	                    		}
             				}
-            			
             		});
             	
-               
+               //Deplacé le noeud traité de liste des noeuds non-visités vers la liste des noeuds visités
                 visited.put(thisNode,nonVisited.get(thisNode));
                 nonVisited.remove(thisNode);
             }
             
         }
+        //Affichage du résultat (distance minimale depuis le noeud source vers tous les noeuds)
         visited.forEach((n,i)
                 -> System.out.println("Sommet : " +source + " vers " + n + " = " + i ));
-
     }
     
     
 
+    //Fonction pour extraire le noeud avec le poids minimun
     public Node getMinNode(HashMap<Node,Integer> nodes){
     	
-    	HashMap<Node, Integer> r = new HashMap<Node, Integer>();
-    	//Integer min = Collections.min(nodes.values());
+    	//HashMap<Node, Integer> r = new HashMap<Node, Integer>();
     	Entry<Node, Integer> x = null;
-    	
-    	/*for(Entry<Node, Integer> node : nodes.entrySet()) {
-    		if(node.getValue().equals(min)) {
-    			//r.put(node.getKey(), node.getValue());
-    			 minNode = node.getKey();
-    		}else {
-    			minNode = null;
-    			} 
-    	}**/
-    	
+        //Chercher la plus petite valeur dans la HashMap
     	for (Entry<Node, Integer> node : nodes.entrySet()) {
     	    if (x == null || x.getValue() > node.getValue()) {
     	        x = (Entry<Node, Integer>) node;
     	    }
     	}
-    	
+        //Renvoyer le Noeud avec la valeur minimale
 		return  x.getKey();
     }
     
